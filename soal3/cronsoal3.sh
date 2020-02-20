3866d577b630c5c34f868df5a49a0a259709600e
zmahmudi <z0mahmudi@gmail.com>
2020-02-20 14:16:07 +0700

    Script Cronjob, tidak untuk dicoba

---
+++ soal3/cronsoal3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+0 06 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
+0 14 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
+0 22 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
