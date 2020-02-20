4837facccef4f289016df083070d8c6e6386d48b
zmahmudi <52489034+zmahmudi@users.noreply.github.com>
2020-02-20 14:27:39 +0700

    Delete cronsoal3.sh

--- soal3/cronsoal3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-0 06 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
-0 14 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
-0 22 * * * ls /home/mahmud/praktikum1/soal3/soal3.sh > /home/mahmud/praktikum1/kenangan
